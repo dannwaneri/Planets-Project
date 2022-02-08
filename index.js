1df5048ce35afb61e9db35de489c8b7504521426
Penuelnwaneri <penuelchudi@gmail.com>
2022-02-08 03:05:22 +0100

    Habitable planets filter

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const {parse} = require('csv-parse');
 const fs = require('fs');
 
-const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanets(planet) {
+return planet['koi_disposition'] === 'CONFIRMED'
+&& planet['koi_insol'] > 0.36 
+&& planet['koi_insol'] < 1.11
+&& planet['koi_prad'] < 1.6
+}
 
 fs.createReadStream('kepler_data.csv')
 .pipe(parse({
@@ -9,14 +16,15 @@ fs.createReadStream('kepler_data.csv')
     columns:true,
 }))
 .on('data', (data) => {
-results.push(data);
+    if(isHabitablePlanets(data)){
+        habitablePlanets.push(data); 
+    }
 })
 .on('error', (err) => {
     console.error(err);
 })
 .on('end', () => {
-    console.log(results)
-    console.log('done')
+console.log(`${habitablePlanets.length} habitable planets found!`)
 });
 // parse();
 
